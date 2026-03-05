5325023c489494ed6c17071bbb3f42677f006365
Roman <roman.peskoff@gmail.com>
2026-03-06 00:20:10 +0300

    remove extra

--- OOP_laba4/Form2.Designer.cs
+++ OOP_laba4/Form2.Designer.cs
@@ -33,7 +33,6 @@ partial class Form2
     {
         button_Back = new System.Windows.Forms.Button();
         button_Exit = new System.Windows.Forms.Button();
-        dataGridView1 = new System.Windows.Forms.DataGridView();
         Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
         Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
         Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,29 +41,16 @@ partial class Form2
         Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
         Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
         Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-        textBox_Actions = new System.Windows.Forms.TextBox();
-        numericUpDown_Obj = new System.Windows.Forms.NumericUpDown();
-        button_CreateObj = new System.Windows.Forms.Button();
-        button_DeleteObj = new System.Windows.Forms.Button();
-        listView1 = new System.Windows.Forms.ListView();
-        columnHeader1 = new System.Windows.Forms.ColumnHeader();
-        columnHeader2 = new System.Windows.Forms.ColumnHeader();
-        columnHeader3 = new System.Windows.Forms.ColumnHeader();
-        columnHeader4 = new System.Windows.Forms.ColumnHeader();
-        button_Measure = new System.Windows.Forms.Button();
-        label1 = new System.Windows.Forms.Label();
-        label2 = new System.Windows.Forms.Label();
-        button_CreatePremiumObj = new System.Windows.Forms.Button();
         textBox_FactoryObj = new System.Windows.Forms.TextBox();
         button_CreateRndFam = new System.Windows.Forms.Button();
         button_CreatePremFam = new System.Windows.Forms.Button();
-        ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-        ((System.ComponentModel.ISupportInitialize)numericUpDown_Obj).BeginInit();
+        textBox_Flyweight = new System.Windows.Forms.TextBox();
+        button_Flyweight = new System.Windows.Forms.Button();
         SuspendLayout();
         // 
         // button_Back
         // 
-        button_Back.Location = new System.Drawing.Point(3, 891);
+        button_Back.Location = new System.Drawing.Point(12, 633);
         button_Back.Name = "button_Back";
         button_Back.Size = new System.Drawing.Size(115, 35);
         button_Back.TabIndex = 0;
@@ -74,23 +60,13 @@ partial class Form2
         // 
         // button_Exit
         // 
-        button_Exit.Location = new System.Drawing.Point(865, 891);
+        button_Exit.Location = new System.Drawing.Point(865, 633);
         button_Exit.Name = "button_Exit";
         button_Exit.Size = new System.Drawing.Size(115, 35);
         button_Exit.TabIndex = 1;
         button_Exit.Text = "Выход";
         button_Exit.UseVisualStyleBackColor = true;
         button_Exit.Click += button_Exit_Click;
-        // 
-        // dataGridView1
-        // 
-        dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8 });
-        dataGridView1.Location = new System.Drawing.Point(12, 34);
-        dataGridView1.Name = "dataGridView1";
-        dataGridView1.Size = new System.Drawing.Size(630, 192);
-        dataGridView1.TabIndex = 2;
-        dataGridView1.Text = "dataGridView1";
         // 
         // Column1
         // 
@@ -132,119 +108,9 @@ partial class Form2
         Column8.HeaderText = "Количество работников";
         Column8.Name = "Column8";
         // 
-        // textBox_Actions
-        // 
-        textBox_Actions.Location = new System.Drawing.Point(673, 34);
-        textBox_Actions.Multiline = true;
-        textBox_Actions.Name = "textBox_Actions";
-        textBox_Actions.ReadOnly = true;
-        textBox_Actions.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-        textBox_Actions.Size = new System.Drawing.Size(284, 191);
-        textBox_Actions.TabIndex = 3;
-        // 
-        // numericUpDown_Obj
-        // 
-        numericUpDown_Obj.Location = new System.Drawing.Point(43, 253);
-        numericUpDown_Obj.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
-        numericUpDown_Obj.Minimum = new decimal(new int[] { 1000, 0, 0, -2147483648 });
-        numericUpDown_Obj.Name = "numericUpDown_Obj";
-        numericUpDown_Obj.Size = new System.Drawing.Size(249, 23);
-        numericUpDown_Obj.TabIndex = 4;
-        // 
-        // button_CreateObj
-        // 
-        button_CreateObj.Location = new System.Drawing.Point(324, 253);
-        button_CreateObj.Name = "button_CreateObj";
-        button_CreateObj.Size = new System.Drawing.Size(173, 23);
-        button_CreateObj.TabIndex = 5;
-        button_CreateObj.Text = "Создать объект";
-        button_CreateObj.UseVisualStyleBackColor = true;
-        button_CreateObj.Click += button_CreateObj_Click;
-        // 
-        // button_DeleteObj
-        // 
-        button_DeleteObj.Location = new System.Drawing.Point(728, 251);
-        button_DeleteObj.Name = "button_DeleteObj";
-        button_DeleteObj.Size = new System.Drawing.Size(173, 25);
-        button_DeleteObj.TabIndex = 6;
-        button_DeleteObj.Text = "Удалить объект";
-        button_DeleteObj.UseVisualStyleBackColor = true;
-        button_DeleteObj.Click += button_DeleteObj_Click;
-        // 
-        // listView1
-        // 
-        listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
-        listView1.Location = new System.Drawing.Point(12, 292);
-        listView1.Name = "listView1";
-        listView1.Size = new System.Drawing.Size(945, 220);
-        listView1.TabIndex = 7;
-        listView1.UseCompatibleStateImageBehavior = false;
-        // 
-        // columnHeader1
-        // 
-        columnHeader1.Name = "columnHeader1";
-        columnHeader1.Text = "Коллекция";
-        columnHeader1.Width = 236;
-        // 
-        // columnHeader2
-        // 
-        columnHeader2.Name = "columnHeader2";
-        columnHeader2.Text = "Вставка";
-        columnHeader2.Width = 236;
-        // 
-        // columnHeader3
-        // 
-        columnHeader3.Name = "columnHeader3";
-        columnHeader3.Text = "Последовательная выборка";
-        columnHeader3.Width = 236;
-        // 
-        // columnHeader4
-        // 
-        columnHeader4.Name = "columnHeader4";
-        columnHeader4.Text = "Случайная выборка";
-        columnHeader4.Width = 236;
-        // 
-        // button_Measure
-        // 
-        button_Measure.Location = new System.Drawing.Point(435, 534);
-        button_Measure.Name = "button_Measure";
-        button_Measure.Size = new System.Drawing.Size(132, 38);
-        button_Measure.TabIndex = 8;
-        button_Measure.Text = "Измерить";
-        button_Measure.UseVisualStyleBackColor = true;
-        button_Measure.Click += button3_Click;
-        // 
-        // label1
-        // 
-        label1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)204));
-        label1.Location = new System.Drawing.Point(274, 6);
-        label1.Name = "label1";
-        label1.Size = new System.Drawing.Size(159, 23);
-        label1.TabIndex = 9;
-        label1.Text = "Объекты";
-        // 
-        // label2
-        // 
-        label2.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)204));
-        label2.Location = new System.Drawing.Point(765, 6);
-        label2.Name = "label2";
-        label2.Size = new System.Drawing.Size(102, 25);
-        label2.TabIndex = 10;
-        label2.Text = "События";
-        // 
-        // button_CreatePremiumObj
-        // 
-        button_CreatePremiumObj.Location = new System.Drawing.Point(530, 253);
-        button_CreatePremiumObj.Name = "button_CreatePremiumObj";
-        button_CreatePremiumObj.Size = new System.Drawing.Size(173, 23);
-        button_CreatePremiumObj.TabIndex = 11;
-        button_CreatePremiumObj.Text = "Создать премиум объект";
-        button_CreatePremiumObj.UseVisualStyleBackColor = true;
-        button_CreatePremiumObj.Click += button_CreatePremiumObj_Click;
-        // 
         // textBox_FactoryObj
         // 
-        textBox_FactoryObj.Location = new System.Drawing.Point(42, 591);
+        textBox_FactoryObj.Location = new System.Drawing.Point(32, 302);
         textBox_FactoryObj.Multiline = true;
         textBox_FactoryObj.Name = "textBox_FactoryObj";
         textBox_FactoryObj.ReadOnly = true;
@@ -254,7 +120,7 @@ partial class Form2
         // 
         // button_CreateRndFam
         // 
-        button_CreateRndFam.Location = new System.Drawing.Point(643, 615);
+        button_CreateRndFam.Location = new System.Drawing.Point(698, 343);
         button_CreateRndFam.Name = "button_CreateRndFam";
         button_CreateRndFam.Size = new System.Drawing.Size(161, 59);
         button_CreateRndFam.TabIndex = 13;
@@ -264,7 +130,7 @@ partial class Form2
         // 
         // button_CreatePremFam
         // 
-        button_CreatePremFam.Location = new System.Drawing.Point(643, 780);
+        button_CreatePremFam.Location = new System.Drawing.Point(698, 498);
         button_CreatePremFam.Name = "button_CreatePremFam";
         button_CreatePremFam.Size = new System.Drawing.Size(161, 59);
         button_CreatePremFam.TabIndex = 14;
@@ -272,63 +138,54 @@ partial class Form2
         button_CreatePremFam.UseVisualStyleBackColor = true;
         button_CreatePremFam.Click += button_CreatePremiumRndFam_Click;
         // 
+        // textBox_Flyweight
+        // 
+        textBox_Flyweight.Location = new System.Drawing.Point(32, 23);
+        textBox_Flyweight.Multiline = true;
+        textBox_Flyweight.Name = "textBox_Flyweight";
+        textBox_Flyweight.ReadOnly = true;
+        textBox_Flyweight.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        textBox_Flyweight.Size = new System.Drawing.Size(524, 246);
+        textBox_Flyweight.TabIndex = 15;
+        // 
+        // button_Flyweight
+        // 
+        button_Flyweight.Location = new System.Drawing.Point(698, 108);
+        button_Flyweight.Name = "button_Flyweight";
+        button_Flyweight.Size = new System.Drawing.Size(161, 56);
+        button_Flyweight.TabIndex = 16;
+        button_Flyweight.Text = "Flyweight demo";
+        button_Flyweight.UseVisualStyleBackColor = true;
+        // 
         // Form2
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         BackColor = System.Drawing.SystemColors.Control;
-        ClientSize = new System.Drawing.Size(992, 938);
+        ClientSize = new System.Drawing.Size(992, 695);
+        Controls.Add(button_Flyweight);
+        Controls.Add(textBox_Flyweight);
         Controls.Add(button_CreatePremFam);
         Controls.Add(button_CreateRndFam);
         Controls.Add(textBox_FactoryObj);
-        Controls.Add(button_CreatePremiumObj);
-        Controls.Add(label2);
-        Controls.Add(label1);
-        Controls.Add(button_Measure);
-        Controls.Add(listView1);
-        Controls.Add(button_DeleteObj);
-        Controls.Add(button_CreateObj);
-        Controls.Add(numericUpDown_Obj);
-        Controls.Add(textBox_Actions);
-        Controls.Add(dataGridView1);
         Controls.Add(button_Exit);
         Controls.Add(button_Back);
         FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
         Location = new System.Drawing.Point(15, 15);
         MaximizeBox = false;
         Text = "OOP3";
-        ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-        ((System.ComponentModel.ISupportInitialize)numericUpDown_Obj).EndInit();
         ResumeLayout(false);
         PerformLayout();
     }
+
+    private System.Windows.Forms.Button button_Flyweight;
+
+    private System.Windows.Forms.TextBox textBox_Flyweight;
 
     private System.Windows.Forms.Button button_CreateRndFam;
     private System.Windows.Forms.Button button_CreatePremFam;
 
     private System.Windows.Forms.TextBox textBox_FactoryObj;
-
-    private System.Windows.Forms.Button button_CreatePremiumObj;
-
-    private System.Windows.Forms.Label label2;
-
-    private System.Windows.Forms.Label label1;
-
-    private System.Windows.Forms.ColumnHeader columnHeader3;
-    private System.Windows.Forms.ColumnHeader columnHeader4;
-
-    private System.Windows.Forms.ColumnHeader columnHeader2;
-
-    private System.Windows.Forms.ColumnHeader columnHeader1;
-
-    private System.Windows.Forms.ListView listView1;
-    private System.Windows.Forms.Button button_Measure;
-
-    private System.Windows.Forms.NumericUpDown numericUpDown_Obj;
-    private System.Windows.Forms.Button button_CreateObj;
-    private System.Windows.Forms.Button button_DeleteObj;
-
-    private System.Windows.Forms.TextBox textBox_Actions;
 
     private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
--- OOP_laba4/Form2.cs
+++ OOP_laba4/Form2.cs
@@ -8,17 +8,14 @@ public partial class Form2 : Form
 {
     private AirportCollection _list;
     
-    private readonly RandomAbstractFactory _factoryRnd = new RandomAbstractFactory();
+    private readonly AbstractFactory _factoryRnd = new RandomAbstractFactory();
     
-    private readonly PremiumAbstractFactory _factoryPrem =  new PremiumAbstractFactory();
+    private readonly AbstractFactory _factoryPrem =  new PremiumAbstractFactory();
     
     private ListEventListener _listener;
     public Form2()
     {
         InitializeComponent();
-        _list = new AirportCollection();
-        _listener = new ListEventListener(_list, textBox_Actions);
-        listView1.View = View.Details;
     }
     private void button_Back_Click(object sender, EventArgs e)
     {
@@ -30,118 +27,6 @@ public partial class Form2 : Form
         Application.Exit();
     }
     
-    private void RefreshObjectGrid()
-    {
-        dataGridView1.Rows.Clear();
-
-        foreach ( Airport airport in _list.List)
-        {
-            int index = _list.List.IndexOf(airport);
-            
-            dataGridView1.Rows.Add(
-                index,
-                airport.Name,
-                airport.Location,
-                airport.FlightsPerDay,
-                airport.TicketsSold,
-                airport.Balance,
-                airport.Rating,
-                airport.EmployeesCount
-            );
-        }
-    }
-
-    private async void button3_Click(object sender, EventArgs e)
-    {
-        button_Measure.Enabled = false;
-        button_Measure.Text = "Загрузка...";
-        listView1.Items.Clear();
-        
-        var (listResults, arrayResults) = await Task.Run(() =>
-        {
-            var listResults = new
-            {
-                Insert = PerformanceMeter.InsertIntoList().ToString(),
-                Seq = PerformanceMeter.MeasureListSequentialRead().ToString(),
-                Rand = PerformanceMeter.MeasureListRandomRead().ToString()
-            };
-
-            var arrayResults = new
-            {
-                Insert = PerformanceMeter.InsertIntoArray().ToString(),
-                Seq = PerformanceMeter.MeasureArraySequentialRead().ToString(),
-                Rand = PerformanceMeter.MeasureArrayRandomRead().ToString()
-            };
-
-            return (listResults, arrayResults);
-        });
-        
-        ListViewItem itemHash = new ListViewItem("List");
-        itemHash.SubItems.AddRange(new[] { listResults.Insert, listResults.Seq, listResults.Rand });
-        listView1.Items.Add(itemHash);
-
-        ListViewItem itemArray = new ListViewItem("Array");
-        itemArray.SubItems.AddRange(new[] { arrayResults.Insert, arrayResults.Seq, arrayResults.Rand });
-        listView1.Items.Add(itemArray);
-
-        button_Measure.Text = "Измерить";
-        button_Measure.Enabled = true;
-    }
-
-    private void button_CreateObj_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            _list.AddRandomItem();
-            RefreshObjectGrid();
-        }
-        catch (Exception exception)
-        {
-            NativeMessageBox.MessageBox(
-                0,
-                exception.Message,
-                "Ошибка",
-                NativeMessageBox.MB_OK | NativeMessageBox.MB_ICONERROR
-            );
-        }
-    }
-
-    private void button_DeleteObj_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            int index = (int)numericUpDown_Obj.Value;
-            _list.Remove(index);
-            RefreshObjectGrid();
-        }
-        catch (Exception exception)
-        {
-            NativeMessageBox.MessageBox(
-                0,
-                exception.Message,
-                "Ошибка",
-                NativeMessageBox.MB_OK | NativeMessageBox.MB_ICONERROR
-            );
-        }
-    }
-
-    private void button_CreatePremiumObj_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            _list.AddRandomPremiumItem();
-            RefreshObjectGrid();
-        }
-        catch (Exception exception)
-        {
-            NativeMessageBox.MessageBox(
-                0,
-                exception.Message,
-                "Ошибка",
-                NativeMessageBox.MB_OK | NativeMessageBox.MB_ICONERROR
-            );
-        }
-    }
 
     private void button_CreateRndFam_Click(object sender, EventArgs e)
     {
